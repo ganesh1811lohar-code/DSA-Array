5d5e64f9ddde3581e958c8ea61f8af88f8751ce5
ganeshlohar1811-GL <ganesh1811lohar@gmail.com>
2026-08-03 11:08:57 +0530

    Main

---
+++ SubArrayInArrays.java
@@ -0,0 +1,20 @@
+class SubArrayInArrays {
+    public static int subArray(int numbers[]) {
+        for(int i=0;i<numbers.length;i++) {
+            int start=i;
+            for(int j=i;j<numbers.length;j++) {
+                int end=j;
+                for(int k=start;k<=end;k++) {
+                    System.out.print(numbers[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,6,7,9};
+        subArray(numbers);
+    }
+}
